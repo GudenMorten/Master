da1f56b2b075067a5752e252e9f08b01248c4378
GudenMorten <morteneriksen96@gmail.com>
2023-01-23 13:41:31 +0100

    get data

--- get_data.py
+++ get_data.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import eikon as ek
+
+# Set the AppKey for the Eikon Data API
+ek.set_app_key('62297b98ae4947f2ac01401801b0c32a165a1053')
+
+# Retrieve data from the Eikon database
+data, _ = ek.get_data('AAPL.O', ['TR.Revenue',])
+
+# Print the data
+print(data)
