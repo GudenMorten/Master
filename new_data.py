e7512b73e94433fcba669c481cebe5acdefa1a3f
GudenMorten <morteneriksen96@gmail.com>
2023-02-15 16:52:51 +0100

    mekka csv fil med input

--- new_data.py
+++ new_data.py
@@ -7,14 +7,18 @@ ek.set_app_key('022971ea1eb44687b0bcb92bad73c9b83ab08a9d')
 nordic_data = pd.read_excel("total_ISIN.xlsx")
 list1 = nordic_data["International Security Identification Number"].dropna().to_list()
 
-
-df2, _ = ek.get_data(list1, fields=['TR.F.STDebtCurrPortOfLTDebt.date', 'TR.F.STDebtCurrPortOfLTDebt',
+df2, _ = ek.get_data(list1, fields=['TR.CommonName', 'TR.F.STDebtCurrPortOfLTDebt.date', 'TR.F.STDebtCurrPortOfLTDebt',
                                     'TR.F.DebtNonConvertLT', 'TR.F.CapLeaseObligLT',
                                     'TR.F.CurrPortOfLTDebtExclCapLease', 'TR.F.CapLeaseCurrPort', 'TR.NAICSSectorCode',
                                     'TR.NAICSSector', 'TR.NAICSSubsectorCode', 'TR.NAICSSubsector',
                                     'TR.NAICSNationalIndustryCode', 'TR.NAICSNationalIndustry',
-                                    'TR.NAICSIndustryGroupCode', 'TR.NAICSIndustryGroup'
+                                    'TR.NAICSIndustryGroupCode', 'TR.NAICSIndustryGroup', 'TR.ExchangeCountry',
+                                    'TR.ExchangeName'
                                     ],
                      parameters={'SDate': '1990-01-01', 'EDate': '2023-02-15', "Frq": "FY"})
 
 print(df2.count())
+
+
+## Store data to csv file
+# df2.to_csv(r"C:\\Users\\morte\\PycharmProjects\\Master\\gathered_data.csv", index=False)
