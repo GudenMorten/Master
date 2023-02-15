8ecdc9173a5f5cd0604abed818e8a5a89488f4cd
GudenMorten <morteneriksen96@gmail.com>
2023-02-15 17:29:57 +0100

    ny dataframe

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
+dataf = pd.read_csv('gathered_data.csv')
 
-NOR_data = pd.read_csv("C:\\Users\\morte\\PycharmProjects\\Master\\CSV\\NOR.csv")
+# fixing the dataframe
+dataf[['Company Common Name', 'NAICS Sector Code', 'NAICS Sector Name',
+    'NAICS Subsector Code', 'NAICS Subsector Name', 'NAICS National Industry Code',
+    'NAICS National Industry Name', 'NAICS Industry Group Code', 'NAICS Industry Group Name',
+    'Country of Exchange', 'Exchange Name']] = dataf.groupby('Instrument')[['Company Common Name', 'NAICS Sector Code',
+    'NAICS Sector Name', 'NAICS Subsector Code', 'NAICS Subsector Name', 'NAICS National Industry Code',
+    'NAICS National Industry Name', 'NAICS Industry Group Code', 'NAICS Industry Group Name',
+    'Country of Exchange', 'Exchange Name']].fillna(method='ffill')
+
