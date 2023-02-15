e85c684e139b98557a6d7805908deb3147d374a7
GudenMorten <morteneriksen96@gmail.com>
2023-02-15 16:24:00 +0100

    oppdatert datainnhenting og bytte av xl fil

--- new_data.py
+++ new_data.py
@@ -4,19 +4,17 @@ import pandas as pd
 # Replace YOUR_APP_KEY with your Eikon API key
 ek.set_app_key('022971ea1eb44687b0bcb92bad73c9b83ab08a9d')
 
-# Define the ISIN identifier for the company
-isin = 'NO0005052605'
-
-# Use the Eikon Data API to retrieve the company data
-# data = ek.get_data(isin, ['TR.Revenue', 'TR.NetIncomeLoss'])
-df, err = ek.get_data(isin, fields=['TR.PriceClose', 'TR.F.DebtTot'],
-                   parameters={'SDate': '1990-01-01', 'EDate': '2022-12-31', "Frq": "Y"}, debug=True)
+nordic_data = pd.read_excel("total_ISIN.xlsx")
+list1 = nordic_data["International Security Identification Number"].dropna().to_list()
 
 
-# Print the DataFrame
-print(df.head(20))
+df2, _ = ek.get_data(list1, fields=['TR.F.STDebtCurrPortOfLTDebt.date', 'TR.F.STDebtCurrPortOfLTDebt',
+                                    'TR.F.DebtNonConvertLT', 'TR.F.CapLeaseObligLT',
+                                    'TR.F.CurrPortOfLTDebtExclCapLease', 'TR.F.CapLeaseCurrPort', 'TR.NAICSSectorCode',
+                                    'TR.NAICSSector', 'TR.NAICSSubsectorCode', 'TR.NAICSSubsector',
+                                    'TR.NAICSNationalIndustryCode', 'TR.NAICSNationalIndustry',
+                                    'TR.NAICSIndustryGroupCode', 'TR.NAICSIndustryGroup'
+                                    ],
+                     parameters={'SDate': '1990-01-01', 'EDate': '2023-02-15', "Frq": "FY"})
 
-nordic_data = pd.read_excel("nordic_data_bonds.xlsx")
-list1 = nordic_data["Preferred RIC"].dropna().to_list()
-df2, _ =  ek.get_data(list1, fields=['TR.PriceClose', 'TR.F.DebtTot'],
-                   parameters={'SDate': '1990-01-01', 'EDate': '2022-12-31', "Frq": "Y"})
+print(df2.count())
