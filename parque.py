5f19c4410f80a0dd8bb6fb898569622f29527489
kristin <kristin.94@hotmail.com>
2023-02-28 11:48:04 +0100

    gg

--- parque.py
+++ parque.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
+gvkey = pd.read_excel("gvkey.xlsx")
+
+gvkeylist = gvkey["gvkey"].to_list()
 
 data4 = pd.read_parquet('capiq_Debt.parquet', engine='pyarrow')
+
+debt = data4[data4["gvkey"].isin(gvkeylist)]
+
+debt.to_parquet("/Users/kristin/PycharmProjects/Master/debt.parquet")
